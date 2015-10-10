63b11445cd1a17a067db0ec829e5060ff19c3be5
Conrado Uraga <bromodachi@toru-no-macbook-air.local>
2015-10-10 09:59:15 +0900

    Initial Commit

---
+++ FIndPicture/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FIndPicture
+//
+//  Created by Conrado Uraga on 2015/10/10.
+//  Copyright © 2015年 Conrado Uraga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ FIndPictureTests/FIndPictureTests.swift
@@ -0,0 +1,36 @@
+//
+//  FIndPictureTests.swift
+//  FIndPictureTests
+//
+//  Created by Conrado Uraga on 2015/10/10.
+//  Copyright © 2015年 Conrado Uraga. All rights reserved.
+//
+
+import XCTest
+@testable import FIndPicture
+
+class FIndPictureTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
